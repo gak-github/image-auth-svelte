dbcfac2dbb184251615014e5f76bd3b48b29fcf7
Asokumar Gurusamy <Asokumar Gurusamy>
2021-06-25 10:49:05 -0700

    fixing a typo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,4 @@
 <script>
 </script>
-<h3 className='top-head'> An image based based authentication demo</h3>
+<h3 className='top-head'> An image based authentication demo</h3>
 <h2>SignUp/SignIn</h2>
