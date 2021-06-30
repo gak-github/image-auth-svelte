27f3b4582ada2ea86ac39105e6b83a8029b7add7
Asokumar Gurusamy <Asokumar Gurusamy>
2021-06-30 15:15:33 -0700

    error message content change

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -13,7 +13,7 @@
 {:else if $store.isLoginSubmit && !$store.isAuthenticated }
     <div class="container">
         <div class='error'>
-            <h5> Invalid Image-Combination</h5>
+            <h5> Invalid email and image combination.</h5>
         </div>
         <Signin />
     </div>
